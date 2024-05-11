71d21f233b1f6ceb8a3363ba8fc130fdf2b9a1f1
mostafa kamal <elmmtaz44@gmail.com>
2024-05-11 14:03:35 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom';
 import Home from "./component/home/Home";
 import EmptyCart from "./component/emptyCart/EmptyCart1";
 import Navbars from "./component/navbar/Navbars";
 import ContactUs from "./component/contact/ContactUs";
 import Footer from "./component/footer/Footer";
 import NotFound from "./component/not_found/NotFound";
+
 function App() {
 
-  return(
-    
-    <>
-      <Navbars />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<EmptyCart />} />
-      </Routes>
-      <ContactUs />
-      <Footer />
-    </>
+  return (
 
-  )
+    <Router>
+
+      <Navbars />
+
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/cart" element={<EmptyCart />} />
+
+        <Route path="/*" element={<NotFound />} />
+
+        <Redirect from="*" to="/" />
+
+      </Routes>
+
+      <ContactUs />
+
+      <Footer />
+
+    </Router>
+  );
+  
 }
 
 export default App;
